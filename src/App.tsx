cce8864fed29167c8daba8e462bc83962a2898e3
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-05-16 09:09:48 +0530

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -2,63 +2,116 @@ import "./App.css";
 import "@xyflow/react/dist/style.css";
 import "yadl-preview/dist/yadl-preview.css";
 import "react-fontpicker-ts/dist/index.css";
-import { Preview, SearchComponents } from "yadl-preview";
+import { Preview } from "yadl-preview";
 import { YadlEditor, YadlEditorRef, YadlEditorResponse } from "yadl-editor";
 import { Allotment } from "allotment";
 import "./allotment.css";
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { Edge, Node } from "@xyflow/react";
+import { debounce } from "lodash";
 
 function App() {
   const [currentNodes, setCurrentNodes] = useState<Node[]>([]);
   const [currentEdges, setCurrentEdges] = useState<Edge[]>([]);
   const editorReference = useRef<YadlEditorRef | null>(null);
+  const [codeVisible, setCodeVisible] = useState(true);
+  const [previewVisible, setPreviewVisible] = useState(true);
+  const [hasReadFromLocalStorage, setHasReadFromLocalStorage] = useState(false);
+  const [sizes, setSizes] = useState<number[]>();
+
+
+  const handleChange = useMemo(
+    () =>
+      debounce((sizes: any) => {
+        localStorage.setItem("sizes", JSON.stringify(sizes));
+      }, 100),
+    []
+  );
+
+  useEffect(() => {
+    const value = localStorage.getItem("sizes");
+
+    if (value) {
+      setSizes(JSON.parse(value));
+    }
+
+    setHasReadFromLocalStorage(true);
+  }, []);
+
 
   return (
     <div>
+      <div>
+        <button
+          className="btn btn-ghost"
+          onClick={() => {
+            setCodeVisible((codeVisible) => !codeVisible);
+          }}
+        >
+          Code
+        </button>
+        <button
+          className="btn btn-ghost"
+          onClick={() => {
+            setPreviewVisible((previewVisible) => !previewVisible);
+          }}
+        >
+          Preview
+        </button>
+        <button className="btn btn-ghost">Components</button>
+      </div>
       <div style={{ display: "grid", gridTemplateColumns: "1fr" }}>
         <div style={{ minHeight: "100vh", width: "100%" }}>
-          <Allotment>
-            <YadlEditor
-              ref={editorReference}
-              onChange={(code: YadlEditorResponse) => {
-                setCurrentNodes(code.nodes as Node[]);
-                setCurrentEdges(code.edges as Edge[]);
-              }}
-              code={`
+          {hasReadFromLocalStorage &&
+            <Allotment
+              snap
+              defaultSizes={sizes}
+              onChange={handleChange}
+            >
+              <Allotment.Pane visible={codeVisible}>
+                <div style={{ height: "100vh", width: "100%" }}>
+                  <YadlEditor
+                    ref={editorReference}
+                    onChange={(code: YadlEditorResponse) => {
+                      setCurrentNodes(code.nodes as Node[]);
+                      setCurrentEdges(code.edges as Edge[]);
+                    }}
+                    code={`
 aws-icon aWSAnalyticsAthena { position { x: 0 y: 100 } dimension { width: 100 height: 100 } }
 
 azure-icon azureAiMachineLearningAIStudio  { position { x: 0 y: 200 } }
 `}
-            />
-            <div style={{ height: "100vh", width: "100%" }}>
-              <Preview
-                initialNodes={currentNodes}
-                initialEdges={currentEdges}
-                onNodeSelect={(node) => {
-                  editorReference.current?.onNodeSelect(node);
-                }}
-                onEdgeConnect={(edge: any) => {
-                  editorReference.current?.onEdgeConnect(edge);
-                }}
-                onNodePositionChanged={(node: any) => {
-                  editorReference.current?.onNodePositionChanged(node);
-                }}
-                onNodeRemoved={(node: any) => {
-                  editorReference.current?.onNodeRemoved(node);
-                }}
-                onNodeResized={(node: any) => {
-                  editorReference.current?.onNodeResized(node);
-                }}
-                onNodeAdded={(node: any) => {
-                  editorReference.current?.onNodeAdded(node);
-                }}
-              />
-            </div>
-            {/* <div style={{ height: "100%", width: "100%" }}>
-              <SearchComponents />
-            </div> */}
-          </Allotment>
+                  />
+                </div>
+              </Allotment.Pane>
+              <Allotment.Pane visible={previewVisible}>
+                <div style={{ height: "100vh", width: "100%" }}>
+                  <Preview
+                    initialNodes={currentNodes}
+                    initialEdges={currentEdges}
+                    onNodeSelect={(node) => {
+                      editorReference.current?.onNodeSelect(node);
+                    }}
+                    onEdgeConnect={(edge: any) => {
+                      editorReference.current?.onEdgeConnect(edge);
+                    }}
+                    onNodePositionChanged={(node: any) => {
+                      editorReference.current?.onNodePositionChanged(node);
+                    }}
+                    onNodeRemoved={(node: any) => {
+                      editorReference.current?.onNodeRemoved(node);
+                    }}
+                    onNodeResized={(node: any) => {
+                      editorReference.current?.onNodeResized(node);
+                    }}
+                    onNodeAdded={(node: any) => {
+                      editorReference.current?.onNodeAdded(node);
+                    }}
+                  />
+                </div>
+              </Allotment.Pane>
+            </Allotment>
+          }
         </div>
       </div>
     </div>
