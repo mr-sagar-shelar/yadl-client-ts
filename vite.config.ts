ec29d67ad2c513d6ac0e67fef22875ecc0337f49
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-05-19 19:50:27 +0530

    Added compression option

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { compression } from 'vite-plugin-compression2'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/yadl-client-ts",
   plugins: [react(),
-  tailwindcss()
+  tailwindcss(),
+  compression()
   ],
 })
