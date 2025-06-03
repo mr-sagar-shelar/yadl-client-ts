d2c65c41105ab66ce19645a9356ea70cccc44d1d
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-06-03 20:38:04 +0530

    Integrated author component

--- src/components/search/search-author-components.tsx
+++ src/components/search/search-author-components.tsx
@@ -54,7 +54,7 @@ const SearchTextComponents = () => {
                 );
             });
         return listItems;
-    }, [currentAuthorNameText, currentAuthorCaptionText]);
+    }, [currentAuthorNameText, currentAuthorCaptionText, currentAuthorImageSource]);
 
     return (
         <>
