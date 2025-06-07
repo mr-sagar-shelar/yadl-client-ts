8550429f3cf87ef144fd1d82144fae80b4f2a702
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-06-07 19:53:19 +0530

    Added logo

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -3,11 +3,18 @@
 import { HeaderMenu } from "./layout/header-menu"
 import { PresetShare } from "./layout/preset-share"
 import { ThemeToggle } from "./layout/mode-toggle"
+import { useTheme } from "./theme-provider"
 
 export function SiteHeader() {
+  const { theme } = useTheme()
+
   return (
     <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b">
-      <div className="flex h-(--header-height) w-full items-center gap-2 px-4">
+      <a href="https://yadl.in">
+        <img className="img img-dark pl-2 link" width="100"
+          src={`/images/logo${theme == "dark" ? "-darkmode" : ""}.png`} alt="YADL" />
+      </a>
+      <div className="flex h-(--header-height) w-full items-center gap-2 pr-4">
         <HeaderMenu />
         <div className="ml-auto px-3 flex gap-4">
           <PresetShare />
