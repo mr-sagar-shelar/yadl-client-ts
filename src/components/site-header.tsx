803ecf4d7579b987a66d230dca8b1c40882b5a3f
mr-sagar-shelar <shaggy.shelar@gmail.com>
2025-06-09 18:51:29 +0530

    minor fix

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -3,17 +3,17 @@
 import { HeaderMenu } from "./layout/header-menu"
 import { PresetShare } from "./layout/preset-share"
 import { ThemeToggle } from "./layout/mode-toggle"
-import { useTheme } from "./theme-provider"
+// import { useTheme } from "./theme-provider"
 
 export function SiteHeader() {
-  const { theme } = useTheme()
+  // const { theme } = useTheme()
 
   return (
     <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b print:hidden">
-      <a href="https://yadl.in">
+      {/* <a href="https://yadl.in">
         <img className="img img-dark pl-2 link" width="100"
           src={`/yadl-client-ts/images/logo${theme == "dark" ? "-darkmode" : ""}.png`} alt="YADL" />
-      </a>
+      </a> */}
       <div className="flex h-(--header-height) w-full items-center gap-2 pr-4">
         <HeaderMenu />
         <div className="ml-auto px-3 flex gap-4">
